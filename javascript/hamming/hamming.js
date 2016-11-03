78a1a31d6ca8dffba81037ab8c0ca9655f0484ef
kimkvn <kimkvn@gmail.com>
2016-11-02 23:35:29 -0400

    hamming: test 8, final, passed. TIL about the 'throw' statement of a function in js.

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,14 @@
 var Hamming = function(){};
 
 Hamming.prototype.compute = function(first, second){
+
   var count = 0;
-  if(first !== second){
+
+  if(first.length != second.length){
+    throw Error("DNA strands must be of equal length.");
+  }
+
+  else if(first !== second){
     for(var i = 0; i < first.length; i++){
       if(first[i] !== second[i]){
         count++;
@@ -11,10 +17,7 @@ Hamming.prototype.compute = function(first, second){
     return count;
   }
 
-  // if(first !== second){
-  //   return first.length;
-  // }
-  if(first === second){
+  else{
     return 0;
   }
 
