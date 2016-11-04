fe82ab5678d5ce435239c4799b73372bcfde4d41
kimkvn <kimkvn@gmail.com>
2016-11-04 02:13:57 -0400

    thought of one solution to check for absence of letters...just gotta retool some of my if statements to pass that last test I think

--- javascript/5-bob/bob.js
+++ javascript/5-bob/bob.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
@@ -20,6 +23,13 @@ Bob.prototype.hey = function(input) {
   }
   if(input[last] === "!"){
     return 'Whatever.';
+  }
+  for(var i=0; i<input.length; i++){
+    for(var j=0; j<letters.length; j++){
+      if(input[i] === letters[j]){
+        return 'Whatever.'
+      }
+    }
   }
   return 'Whatever.'
 };
