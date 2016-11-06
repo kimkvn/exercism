0e4c226bc2550b10c35ebc2d39b21d83d8aa7959
kimkvn <kimkvn@gmail.com>
2016-11-06 13:22:26 -0500

    YES found a solution for the 8th test without fucking up the other tests

--- javascript/5-bob/bob.js
+++ javascript/5-bob/bob.js
@@ -4,33 +4,48 @@
 //
 
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
 
 var Bob = function() {};
 
+
+
 Bob.prototype.hey = function(input) {
 
-  if(typeof input === "number"){
+  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var letterCount = 0;
+  var last = input.length - 1;
+
+  for(var i=0; i<input.length; i++){
+    for(var j=0; j<letters.length; j++){
+      if(input[i] === letters[j] || input[i] === letters[j].toUpperCase()){
+          letterCount++;
+      }
+    }
+    break;
+  }
+
+  if(letterCount === 0 && input[last] !== "!"){
     return 'Whatever.';
   }
 
   if(input === input.toUpperCase()){
     return 'Whoa, chill out!';
   }
-  var last = input.length - 1;
+
   if(input[last] === "?"){
     return 'Sure.';
   }
+
+  if(typeof input === "number"){
+    return 'Whatever.';
+  }
+
   if(input[last] === "!"){
     return 'Whatever.';
   }
-  for(var i=0; i<input.length; i++){
-    for(var j=0; j<letters.length; j++){
-      if(input[i] === letters[j]){
-        return 'Whatever.'
-      }
-    }
-  }
+
   return 'Whatever.'
 };
 
