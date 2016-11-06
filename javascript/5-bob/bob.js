ccb1981c08933e576160627733e0edad85bf9a18
kimkvn <kimkvn@gmail.com>
2016-11-06 13:58:30 -0500

    all tests passed

--- javascript/5-bob/bob.js
+++ javascript/5-bob/bob.js
@@ -13,6 +13,8 @@ var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
 
+  if(input){}
+
   var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var letterCount = 0;
   var last = input.length - 1;
@@ -24,6 +26,10 @@ Bob.prototype.hey = function(input) {
       }
     }
     break;
+  }
+
+  if(!input.trim()){
+    return "Fine. Be that way!";
   }
 
   if(letterCount === 0 && input[last] === "?"){
